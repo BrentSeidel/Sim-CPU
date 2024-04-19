e8c2569268e524525e6399805593fe83096ea924
BrentSeidel <brentseidel@mac.com>
2024-04-19 11:32:07 -0700

    Move LEA/PEA tests into main test script.

--- test/m68000/test.lisp
+++ test/m68000/test.lisp
@@ -1699,6 +1699,81 @@ lisp
 (test-reg 17 #x1030)
 (test-meml #x1ffc #x102a)
 ;-------------------------------------------------------------------------------
+;  Test LEA/PEA instructions
+;
+;  Load memory
+;
+(memw #x1000 #x41f8) ; LEA START,A0
+(memw #x1002 #x1000)
+(memw #x1004 #x43d0) ; LEA (A0),A1
+(memw #x1006 #x45e9) ; LEA 2(A1),A2
+(memw #x1008 #x0002)
+(memw #x100a #x47fa) ; LEA START(PC),A3
+(memw #x100c #xfff4)
+(memw #x100e #x5440) ; ADDQ #2,D0
+(memw #x1010 #x49f1) ; LEA 2(A1,D0),A4
+(memw #x1012 #x0002)
+(memw #x1014 #x4bfb) ; LEA START(PC,D0),A5
+(memw #x1016 #x00ea)
+;
+(memw #x1018 #x2e7c) ; MOVE.L #SUPER,A7
+(meml #x101a #x00001300)
+(memw #x101e #x207c) ; MOVE.L #USER,A0
+(meml #x1020 #x00001200)
+(memw #x1024 #x4e60) ; MOVE A0,USP
+(memw #x1026 #x41f8) ; LEA START,A0
+(memw #x1028 #x1000)
+;
+(memw #x102a #x4850) ; PEA (A0)
+(memw #x102c #x0a7c) ; EORI #$2000,SR
+(memw #x102e #x2000)
+(memw #x1030 #x4850) ; PEA (A0)
+;
+;  Execute test
+;
+(print "==> Testing LEA/PEA instructions")
+(terpri)
+(sim-init)
+(go #x1000)
+(print "Testing LEA instruction")
+(terpri)
+(sim-step) ; LEA START,A0
+(test-reg 8 #x1000)
+(sim-step) ; LEA (A0),A1
+(test-reg 9 #x1000)
+(sim-step) ; LEA 2(A1),A2
+(test-reg 10 #x1002)
+(sim-step) ; LEA START(PC),A3
+(test-reg 11 #x1000)
+(sim-step) ; ADDQ #2,D0
+(test-reg 0 2)
+(sim-step) ; LEA 2(A1,D0),A4
+(test-reg 12 #x1004)
+(sim-step) ; LEA START(PC,D0),A5
+(test-reg 13 #x1002)
+;
+(print "Setting up stack pointers")
+(terpri)
+(sim-step) ; MOVE.L #SUPER,A7
+(test-reg 16 #x1300) ; Test SSP
+(sim-step) ; MOVE.L #USER,A0
+(test-reg 8 #x1200)
+(sim-step) ; MOVE A0,USP
+(test-reg 15 #x1200) ; Test USP
+(sim-step) ; LEA START,A0
+(test-reg 8 #x1000)
+;
+(print "Testing PEA instruction")
+(terpri)
+(sim-step) ; PEA (A0)
+(test-reg 16 #x12fc)
+(test-meml #x12fc #x00001000)
+(sim-step) ; EORI #$2000,SR
+(test-mask #x0000 #xf000) ; Drop privileges
+(sim-step) ; PEA (A0)
+(test-reg 15 #x11fc)
+(test-meml #x12fc #x00001000)
+;-------------------------------------------------------------------------------
 ;  End of test cases
 ;
 (print "===> Testing complete")
