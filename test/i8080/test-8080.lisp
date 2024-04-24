bc3bcd99f693cac1813818922c29a10eccab8582
BrentSeidel <brentseidel@mac.com>
2024-04-24 14:49:10 -0700

    Convert tests for ADD and ADC instructions to Lisp and add to main test script.

--- test/i8080/test-8080.lisp
+++ test/i8080/test-8080.lisp
@@ -44,9 +44,11 @@ lisp
   (terpri))
 ;
 ;  Check a masked status register value
+;  Status register bits are S|Z|0|AC|0|P|1|C
+;                           7 6 5  4 3 2 1 0
 ;
 (defun test-mask (expected mask)
-  (print "CCR expected ")
+  (print "PSW expected ")
   (print-hex expected)
   (print ", masked ")
   (print-hex (and expected mask))
@@ -85,6 +87,20 @@ lisp
     (progn (setq *FAIL-COUNT* (+ *FAIL-COUNT* 1)) (print " *** FAIL ***")))
   (terpri))
 ;
+;  Check a byte in memory
+;
+(defun test-memb (address expected)
+  (print "Memory ")
+  (print-hex address)
+  (print " expected ")
+  (print-hex expected)
+  (print " actual ")
+  (print-hex (memb address))
+  (if (= expected (memb address))
+    (progn (setq *PASS-COUNT* (+ *PASS-COUNT* 1)) (print " PASS"))
+    (progn (setq *FAIL-COUNT* (+ *FAIL-COUNT* 1)) (print " *** FAIL ***")))
+  (terpri))
+;
 ;  Print summary results
 ;
 (defun summary ()
@@ -96,3 +112,153 @@ lisp
 ;===============================================================================
 ;  Start of test cases
 ;
+;-------------------------------------------------------------------------------
+;  Test ADD/ADC instructions
+;
+; Load memory
+;
+;  Initialize registers
+(memw #x0100 #x06de) ; MVI B,DE
+(memw #x0102 #x0ead) ; MVI C,AD
+(memw #x0104 #x16be) ; MVI D,BE
+(memw #x0106 #x1eef) ; MVI E,EF
+(memw #x0108 #x2610) ; MVI H,10
+(memw #x010a #x2e01) ; MVI L,01
+(memw #x010c #x3612) ; MVI M,12
+(memw #x010e #x3e34) ; MVI A,34
+;
+;  Test ADD x instructions
+(memb #x0110 #x80) ; ADD B
+(memb #x0111 #x81) ; ADD C
+(memb #x0112 #x82) ; ADD D
+(memb #x0113 #x83) ; ADD E
+(memb #x0114 #x84) ; ADD H
+(memb #x0115 #x85) ; ADD L
+(memb #x0116 #x86) ; ADD M (address in HL (12A), contents 0E)
+(memb #x0117 #x87) ; ADD A
+;
+;  Test ADC x instructions
+(memb #x0118 #x88) ; ADC B
+(memb #x0119 #x89) ; ADC C
+(memb #x011a #x8a) ; ADC D
+(memb #x011b #x8b) ; ADC E
+(memb #x011c #x8c) ; ADC H
+(memb #x011d #x8d) ; ADC L
+(memb #x011e #x8e) ; ADC M (address in HL (12A), contents 0E)
+(memb #x011f #x8f) ; ADC A
+;
+;  Execute test
+;
+(print "==> Testing ADD/ADC instructions")
+(terpri)
+(sim-init)
+(go #x0100)
+(sim-step) ; MVI B,DE
+(test-reg RB #xde)
+(sim-step) ; MVI C,AD
+(test-reg RC #xad)
+(sim-step) ; MVI D,BE
+(test-reg RD #xbe)
+(sim-step) ; MVI E,EF
+(test-reg RE #xef)
+(sim-step) ; MVI H,10
+(test-reg RH #x10)
+(sim-step) ; MVI L,01
+(test-reg RL #x01)
+(sim-step) ; MVI M,12
+(test-memb #x1001 #x12)
+(sim-step) ; MVI A,34
+(test-reg RA #x34)
+;
+(print "Testing ADD instructions")
+(terpri)
+(sim-step) ; Verify ADD B
+; Verify that A is 12, PC is 111, A,P,&C flags are set
+(test-reg ra #x12)
+(test-reg rpc #x0111)
+(test-reg rpsw #x17)
+(sim-step) ; Verify ADD C
+; Verify that A is BF, PC is 112, S flag is set
+(test-reg ra #xbf)
+(test-reg rpc #x0112)
+(test-reg rpsw #x82)
+(sim-step) ; Verify ADD D
+; Verify that A is 7D, PC is 113, A,P,&C flags are set
+(test-reg RA #x7d)
+(test-reg RPC #x113)
+(test-reg RPSW #x17)
+(sim-step) ; Verify ADD E
+; Verify that A is 6C, PC is 114, A,P,&C flags are set
+(test-reg RA #x6c)
+(test-reg RPC #x114)
+(test-reg RPSW #x17)
+(sim-step) ; Verify ADD H
+; Verify that A is 7C, PC is 115, no flags are set
+(test-reg RA #x7c)
+(test-reg RPC #x115)
+(test-reg RPSW #x02)
+(sim-step) ; Verify ADD L
+; Verify that A is 7D, PC is 116, P flag is set
+(test-reg RA #x7d)
+(test-reg RPC #x116)
+(test-reg RPSW #x06)
+(sim-step) ; Verify ADD M
+; Verify that A is 8F, PC is 117, S flag is set
+(test-reg RA #x8f)
+(test-reg RPC #x117)
+(test-reg RPSW #x82)
+(sim-step) ; Verify ADD A
+; Verify that A is 1E, PC is 118, A,P,&C flags are set
+(test-reg RA #x1e)
+(test-reg RPC #x118)
+(test-reg RPSW #x17)
+;
+(print "Verifying ADC instructions")
+(terpri)
+(sim-step) ; Verify ADC B
+; Verify that A is FD, PC is 119, S&A flags are set
+(test-reg RA #xfd)
+(test-reg RPC #x119)
+(test-reg RPSW #x92)
+(sim-step) ; Verify ADC C
+; Verify that A is AA, PC is 11A, S,A,P,&C flags are set
+(test-reg RA #xaa)
+(test-reg RPC #x11a)
+(test-reg RPSW #x97)
+(sim-step) ; Verify ADC D
+; Verify that A is 69, PC is 11B, A,P,&C flags are set
+(test-reg RA #x69)
+(test-reg RPC #x11b)
+(test-reg RPSW #x17)
+(sim-step) ; Verify ADC E
+; Verify that A is 59, PC is 11C, A,P,&C flags are set
+(test-reg RA #x59)
+(test-reg RPC #x11c)
+(test-reg RPSW #x17)
+(sim-step) ; Verify ADC H
+; Verify that A is 6A, PC is 11D, P flag is set
+(test-reg RA #x6a)
+(test-reg RPC #x11d)
+(test-reg RPSW #x06)
+(sim-step) ; Verify ADC L
+; Verify that A is 6B, PC is 11E, no flags are set
+(test-reg RA #x6b) ; Actually d4
+(test-reg RPC #x11e)
+(test-reg RPSW #x02)
+(sim-step) ; Verify ADC M
+; Verify that A is 7D, PC is 11F, P flag is set
+(test-reg RA #x7d)
+(test-reg RPC #x11f)
+(test-reg RPSW #x06)
+(sim-step) ; Verify ADC A
+; Verify that A is FA, PC is 120, S,A,&P flags are set
+(test-reg RA #xfa)
+(test-reg RPC #x120)
+(test-reg RPSW #x96)
+;-------------------------------------------------------------------------------
+;  End of test cases
+;
+(print "===> Testing complete")
+(terpri)
+(summary)
+(exit)
