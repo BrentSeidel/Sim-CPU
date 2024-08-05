5de2e2866c362ae40a490792445db6a7caba2b29
BrentSeidel <brentseidel@mac.com>
2024-08-04 19:20:15 -0700

    Prepare loadcpm utility for submittal as alr crate.

--- loadcpm/loadcpm.adb
+++ loadcpm/loadcpm.adb
@@ -16,27 +16,52 @@
 --  You should have received a copy of the GNU General Public License along
 --  with SimCPU. If not, see <https://www.gnu.org/licenses/>.--
 --
+with BBS;
+use type BBS.uint8;
+use type BBS.uint32;
 with BBS.Sim_CPU;
 with BBS.Sim_CPU.disk;
-with test_util;
+with BBS.Sim_CPU.i8080;
 with Ada.Direct_IO;
 with Ada.Text_IO;
 with Ada.Text_IO.Unbounded_IO;
 with Ada.Strings.Unbounded;
 
 procedure LoadCPM is
-   sector_size : constant uint8 := 128;
-   type sector is array (0 .. sector_size - 1) of uint8;
+   sector_size : constant BBS.uint8 := 128;
+   type sector is array (0 .. sector_size - 1) of BBS.uint8;
    package image_file is new Ada.Direct_IO(sector);
    buff   : sector;
    image  : image_file.File_Type;
    fname  : Ada.Strings.Unbounded.Unbounded_String;
    hname  : Ada.Strings.Unbounded.Unbounded_String;
    str    : Ada.Strings.Unbounded.Unbounded_String;
-   start  : uint32;
-   finish : uint32;
-   size   : uint32;
+   start  : BBS.uint32;
+   finish : BBS.uint32;
+   size   : BBS.uint32;
    sect   : Integer := 1;  --  Start at first sector
+   i8080  : aliased BBS.Sim_CPU.i8080.i8080;
+   --
+   --  Pull the next hexidecimal value off of a string
+   --
+   procedure nextValue(v : out BBS.uint32;
+                       s : in out Ada.Strings.Unbounded.Unbounded_String) is
+      first : Ada.Strings.Unbounded.Unbounded_String;
+      rest  : Ada.Strings.Unbounded.Unbounded_String;
+      index : Natural;
+   begin
+      index := ada.Strings.Unbounded.Index(s, " ");
+      if index = 0 then
+         first := s;
+         rest := Ada.Strings.Unbounded.Null_Unbounded_String;
+      else
+         first := Ada.Strings.Unbounded.Unbounded_Slice(s, 1, index - 1);
+         rest := Ada.Strings.Unbounded.Unbounded_Slice(s, index + 1,
+                                                       Ada.Strings.Unbounded.Length(s));
+      end if;
+      v := BBS.Sim_CPU.toHex(Ada.Strings.Unbounded.To_String(first));
+      s := rest;
+   end;
 
 begin
    Ada.Text_IO.Put_Line("Make bootable CP/M disk image");
@@ -47,28 +72,28 @@ begin
 --
    Ada.Text_IO.Put("Enter CP/M Hex file name: ");
    Ada.Text_IO.Unbounded_IO.Get_Line(hname);
-   test_util.cpu.init;
-   test_util.CPU.load(Ada.Strings.Unbounded.To_String(hname));
+   i8080.init;
+   i8080.load(Ada.Strings.Unbounded.To_String(hname));
 --
    Ada.Text_IO.Put("Enter starting address: ");
    Ada.Text_IO.Unbounded_IO.Get_Line(str);
-   test_util.nextValue(start, str);
+   nextValue(start, str);
    Ada.Text_IO.Put("Enter ending address: ");
    Ada.Text_IO.Unbounded_IO.Get_Line(str);
-   test_util.nextValue(finish, str);
+   nextValue(finish, str);
    size := finish - start;
-   Ada.Text_IO.Put_Line("Processing " & uint32'Image(size) &
+   Ada.Text_IO.Put_Line("Processing " & BBS.uint32'Image(size) &
      " bytes of data, or approximately " &
-     uint32'Image((size / uint32(sector_size)) + 1) & " sectors");
+     BBS.uint32'Image((size / BBS.uint32(sector_size)) + 1) & " sectors");
 --
     while start < finish loop
        for x in sector'Range loop
-          buff(x) := uint8(test_util.CPU.Read_Mem(BBS.Sim_CPU.addr_bus(start) +
+          buff(x) := BBS.uint8(i8080.Read_Mem(BBS.Sim_CPU.addr_bus(start) +
              BBS.Sim_CPU.addr_bus(x)));
        end loop;
        image_file.Set_Index(image, image_file.Count(sect));
        image_file.Write(image, buff);
-       start := start + uint32(sector_size);
+       start := start + BBS.uint32(sector_size);
        sect := sect + 1;
        Ada.Text_IO.Put_Line("Sector " & Integer'Image(sect) & " written.");
     end loop;
